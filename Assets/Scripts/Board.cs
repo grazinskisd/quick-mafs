f4983e8b98655b7401f9a4006059585cf33c726e
Dalius Grazinskis <grazinskisd@gmail.com>
2018-09-07 12:47:31 +0200

    Basic board

---
+++ Assets/Scripts/Board.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public int Width;
+    public int Height;
+    public GameObject TilePrefab;
+
+    private GameObject[,] _board;
+
+	private void Start () {
+        _board = new GameObject[Width, Height];
+        InitializeBoard();
+	}
+
+    private void InitializeBoard()
+    {
+        for (int row = 0; row < Width; row++)
+        {
+            for (int col = 0; col < Height; col++)
+            {
+                var tile = Instantiate(TilePrefab);
+                tile.name = string.Format("Tile ({0}, {1})", row, col);
+                tile.transform.SetParent(transform);
+                tile.transform.localPosition = new Vector2(row, col);
+            }
+        }
+    }
+}
