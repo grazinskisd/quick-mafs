6458dcbc7ed9d8d8fa0b294f161b7b581767ad79
Dalius Grazinskis <grazinskisd@gmail.com>
2018-10-08 16:54:51 +0200

    [WIP] tutorial

---
+++ Assets/Scripts/Controllers/TutorialController.cs
@@ -0,0 +1,14 @@
+﻿using Zenject;
+
+namespace QuickMafs
+{
+    public class TutorialController : IInitializable
+    {
+        [Inject] private TutorialView _view;
+
+        public void Initialize()
+        {
+
+        }
+    }
+}
---
+++ Assets/Scripts/Installers/TutorialInstaller.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Zenject;
+
+namespace QuickMafs
+{
+    public class TutorialInstaller : MonoInstaller<MenuInstaller>
+    {
+        public TutorialView TutorialView;
+
+        public override void InstallBindings()
+        {
+            Container.BindInstance(TutorialView);
+
+            Container.BindInterfacesAndSelfTo<TutorialController>().AsSingle();
+        }
+    }
+}
